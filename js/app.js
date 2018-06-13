80f2d725784680c94ba190ff51d94e695058434b
bloom510 <bloomjazzer@gmail.com>
2018-06-13 15:25:11 -0700

    adds scaling to polar coordinates for polygon circumscription

--- js/app.js
+++ js/app.js
@@ -38,12 +38,13 @@ class Canvas {
     getMousePos(){
         const canvas = document.getElementById('canvas')
         const rect = canvas.getBoundingClientRect();
-
+        
         canvas.addEventListener('mousedown', (e) => {
             this.context.moveTo(e.clientX - rect.left, e.clientY - rect.top)
             this.freehand.mousedown = true;
-            // below creates a happy accident. you could generate some dope art by refining the n-gon generator and enabling mouse interactivity
-            // this.polygon(this.draw_history)
+
+            this.newShape(e.clientX - rect.left, e.clientY - rect.top)
+
         });
 
         canvas.addEventListener('mouseup', () => {
@@ -52,7 +53,8 @@ class Canvas {
 
         canvas.addEventListener('mousemove', (e) => {
             if(this.freehand.mousedown){
-                this.paint({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+                this.global.shape.updateRadius(1.01)
+                // this.paint({ x: e.clientX - rect.left, y: e.clientY - rect.top });
             }
         });
     }
@@ -62,52 +64,14 @@ class Canvas {
         this.context.stroke()
     }
 
-    //generates shape for a given array of vertices. 
-    //try and code up a n-gon generator.
-    polygon(vertices){      
-        const context = this.context;
-        context.beginPath();
-
-        for (let i = 0; i < vertices.length; i++){
-            for(let j = 0; j < Object.keys(vertices[i]).length; j += 2){
-                if(i === 0){
-                    context.moveTo(vertices[i][Object.keys(vertices[i])[j]], vertices[i][Object.keys(vertices[i])[j + 1]] );            
-                } else {
-                    context.lineTo(vertices[i][Object.keys(vertices[i])[j]], vertices[i][Object.keys(vertices[i])[j + 1]] );                       
-                    if(i === vertices.length - 1 ) {
-                        context.lineTo(vertices[0][Object.keys(vertices[0])[0]], vertices[0][Object.keys(vertices[0])[1]] )
-                    }
-                }
-            }
-          
-        }
-        context.closePath(); 
-        context.stroke(); 
+    newShape(x, y){
+        this.global.shape = new Shape(this.context, x, y, 50)
     }
 
 }
 
 window.addEventListener('load', () => {
     const canvas = new Canvas(window.innerWidth, window.innerHeight);
-    canvas.init();
+    canvas.init(); 
 });
-
-
-    /*
-    //free-code a triangle
-    let a_triangle = [60, canvas.height / 2, 116, (canvas.height/2) - 70, 172, canvas.height / 2]
-    canvas.global.triangle = new Shape(canvas.context, a_triangle)
-    canvas.draw(canvas.global.triangle.vertices)
-
-    //show the centroid of said triangle (needs testing with other polygons and geometries to see if we need a different choice of algorithm)
-    let centroid = canvas.global.triangle.computeCentroid(canvas.global.triangle.vertices)
-    canvas.showCentroid(centroid)
-
-    //A simple rectangle.
-    let a_rectangle =  [20,20,150,100];
-    canvas.global.rectangle = new Shape(canvas.context, a_rectangle)
-    canvas.draw(canvas.global.rectangle.vertices)
-    */
-
-
 
--- js/shape.js
+++ js/shape.js
@@ -1,48 +1,120 @@
+/*
+Reformat Shape class to function as a robust n-gon generator
+that works by circumscribing a circle with a radius determined
+by mouse distance.
+*/
 class Shape {
-    constructor(context, vertices){
-        //theoretically could accept an array of coordinates as an argument
+    constructor(context, x, y, radius){
         if(!this.context) this.context = context;
-        this.vertices = [];
-        for (let i = 0; i < vertices.length; i += 2){
-            this.vertices.push({x: vertices[i], y: vertices[i + 1]})
+        if(!this.canvas) this.canvas = canvas;
+
+        this.polarSpace = {
+            radius: radius,
+            x: x,
+            y: y,
+            vertices: []
         }
 
+        this.createPolarSpace()
+
     }
 
-    //draws a rectangle given a set of coordinates
-    rect(canvas, vertices){
-        const context = canvas.context;
-        context.fillRect(vertices[0].x, vertices[0].y, vertices[1].x, vertices[1].y);
+    createPolarSpace(){
+        for (let i = 0; i < 12; i++) {
+            let interval = (Math.PI * 2) / 12;
+            let radianAngle = interval * (i + 9);
+      
+            let x = Math.round(this.polarSpace.x + this.polarSpace.radius * Math.cos(radianAngle));
+            let y = Math.round(this.polarSpace.y + this.polarSpace.radius * Math.sin(radianAngle));
+      
+            
+            this.polarSpace.vertices.push({x, y});
+          }
+
+          for(let j = 0; j < this.polarSpace.vertices.length; j++){
+            this.plotDot(this.polarSpace.vertices[j].x, this.polarSpace.vertices[j].y);
+          }
+
     }
+    
+    //Enlarges or reduces to scale for any Polygon given its centroid
+    updateRadius(scale){
+        const C = this.computeCentroid()
+
+        const clearPrevious = (coords) => {
+            this.context.strokeStyle = 'black'
+            this.context.arc(this.polarSpace.vertices[coords].x, this.polarSpace.vertices[coords].y, 0.5, 0, Math.PI * 2)
+            this.context.stroke()
+        }
+
+        const update = (coords) => {
+            this.context.strokeStyle = 'white'
+            this.polarSpace.vertices[coords].x = scale * (this.polarSpace.vertices[coords].x - C.x) + C.x;
+            this.polarSpace.vertices[coords].y = scale * (this.polarSpace.vertices[coords].y - C.y) + C.y;
+        }
         
-    //Compputes the center of an n-gon. Should be tested on a variety of polygons to determine algorithm choice
+        //update dimensions in memory
+        for(let i = 0; i < this.polarSpace.vertices.length; i++){
+            clearPrevious(i)
+            update(i)
+        }
+
+        //re-plot dots
+        for(let j = 0; j < this.polarSpace.vertices.length; j++){
+            this.plotDot(this.polarSpace.vertices[j].x, this.polarSpace.vertices[j].y);
+        }
+       
+    }
+
+    plotDot(x, y) {
+        this.context.beginPath();
+        this.context.arc(x, y, 0.5, 0, Math.PI * 2);
+        this.context.stroke();
+        this.context.closePath();
+      }
+
+          //Compputes the center of an n-gon. Should be tested on a variety of polygons to determine algorithm choice
     computeCentroid(){
         let sumX = 0, sumY = 0, i = 0;
-        for(i; i < this.vertices.length; i++){
-            sumX += this.vertices[i].x;
-            sumY += this.vertices[i].y;
+        for(i; i < this.polarSpace.vertices.length; i++){
+            sumX += this.polarSpace.vertices[i].x;
+            sumY += this.polarSpace.vertices[i].y;
         }
-        const C = { x: sumX / this.vertices.length, y: sumY / this.vertices.length };
+        const C = { x: sumX / this.polarSpace.vertices.length, y: sumY / this.polarSpace.vertices.length };
         return C;    
     }
 
      //draw a dot given the center of a polygon from computeCentroid()
-     showCentroid(canvas, C){
-        const context = canvas.context;
-        canvas.context.beginPath();
-        canvas.context.arc(C.x,C.y,3,0,2*Math.PI);
-        canvas.context.fill();
-        canvas.context.closePath();
-    }
-      
-    //Enlarges or reduces to scale for any Polygon given its centroid
-    dilate(canvas, scale){
-        const C = this.computeCentroid()
-        for(let i = 0; i < this.vertices.length; i++){
-            this.vertices[i].x = scale * (this.vertices[i].x - C.x) + C.x;
-            this.vertices[i].y = scale * (this.vertices[i].y - C.y) + C.y;
-        }
-        canvas.draw(this.vertices);
+     showCentroid(C){
+        const context = this.context;
+        context.beginPath();
+        context.arc(C.x,C.y,3,0,2*Math.PI);
+        context.fill();
+        context.closePath();
     }
  
+
 }
+
+
+//Interesting old stuff below:
+// ngon(sides, radius){      
+//     const context = this.context;
+//     context.beginPath();
+    
+//     //compute the radius of a circle given drag distance
+//     //circumscribe n-gon inside the circle
+
+//     // for (let i = 0; i < vertices.length; i++){
+//     //     for(let j = 0; j < Object.keys(vertices[i]).length; j += 2){
+//     //         if(i === 0){
+//     //             context.moveTo(vertices[i][Object.keys(vertices[i])[j]], vertices[i][Object.keys(vertices[i])[j + 1]] );            
+//     //         } else {
+//     //             context.lineTo(vertices[i][Object.keys(vertices[i])[j]], vertices[i][Object.keys(vertices[i])[j + 1]] );                       
+//     //             if(i === vertices.length - 1 ) {
+//     //                 context.lineTo(vertices[0][Object.keys(vertices[0])[0]], vertices[0][Object.keys(vertices[0])[1]] )
+//     //             }
+//     //         }
+//     //     }
+//     // }
+// }
